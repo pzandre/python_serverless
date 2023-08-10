4bb3fd99903df71f807b9bfb9efc7620b3bbbdf6
Andre Perez <andremztm@gmail.com>
2023-08-10 13:45:10 -0300

    Missing whitespace

--- api/index.py
+++ api/index.py
@@ -7,5 +7,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(('Hello, world!' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))).encode('utf-8'))
+    self.wfile.write(('Hello, world! ' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))).encode('utf-8'))
     return
